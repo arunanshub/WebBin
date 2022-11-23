d5fa7d076b8e8a593189791f7035558151a81dc3
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-23 16:45:47 +0530

    feat(app): add minifier, db-migrate and CSP enforcer

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,12 +2,22 @@ from __future__ import annotations
 
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+from flask_migrate import Migrate
+from flask_minify import Minify
 from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
 
 from config import CONFIG
 
 db = SQLAlchemy()
 bootstrap = Bootstrap5()
+minify = Minify()
+migrate = Migrate()
+
+csp = {
+    "script-src": ["'self'", "cdn.jsdelivr.net"],
+    "style-src": ["'self'", "cdn.jsdelivr.net"],
+}
 
 
 def config_app(config_name: str) -> Flask:
@@ -24,4 +34,13 @@ def config_app(config_name: str) -> Flask:
     # register extensions
     db.init_app(app)
     bootstrap.init_app(app)
+    migrate.init_app(app, db)
+    # minify.init_app(app)
+
+    # register CSP enforcer
+    Talisman(
+        app,
+        content_security_policy=csp,
+        content_security_policy_nonce_in=["script-src"],
+    )
     return app
