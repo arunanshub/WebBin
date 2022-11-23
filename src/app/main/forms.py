9c0b7f29904b24c007ba7de8098fd95df3ed5328
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-24 00:44:13 +0530

    feat(forms): add form description

--- src/app/main/forms.py
+++ src/app/main/forms.py
@@ -2,23 +2,42 @@ from __future__ import annotations
 
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class DataForm(FlaskForm):
-    text = TextAreaField("Data To Hide", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    text = TextAreaField(
+        "Data To Hide",
+        validators=[DataRequired()],
+        render_kw={"rows": 11},
+        description="What secrets do you want to hide?",
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()],
+        description="Password to protect your retarded secrets.",
+    )
     paste_id = StringField(
-        "Custom Slug",
-        validators=[Length(max=64)],
-        description="Custom Slug or ID for the data. Leave it blank if you "
-        "want me to provide one for you.",
+        "Slug/Paste ID",
+        validators=[
+            Length(min=1, max=32),
+            DataRequired(),
+            Regexp(
+                r"^\S+$",
+                message="No whitespace allowed in slug!",
+            ),
+        ],
+        description="Slug or ID for the data. Edit me if you want a "
+        "custom slug.",
     )
     submit = SubmitField("Hide!")
 
 
 class RevealForm(FlaskForm):
-    text = TextAreaField("Secret message", render_kw={"readonly": True})
+    text = TextAreaField(
+        "Secret message",
+        render_kw={"readonly": True, "rows": 11},
+    )
 
 
 class AskPasswordForm(FlaskForm):
@@ -27,4 +46,4 @@ class AskPasswordForm(FlaskForm):
         description="This password decrypts the secret message",
         validators=[DataRequired()],
     )
-    submit = SubmitField()
+    submit = SubmitField("Unlock the secret!")
