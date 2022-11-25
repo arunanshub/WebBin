f7de95203799f4be18d164ef7e2af10b5024432f
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-25 13:26:43 +0530

    fix(app): force https only if app is running under SSL

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,7 +5,6 @@ from flask_bootstrap import Bootstrap5
 from flask_minify import Minify
 from flask_sqlalchemy import SQLAlchemy
 from flask_talisman import Talisman
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 from config import CONFIG
 
@@ -37,11 +36,9 @@ def config_app(config_name: str) -> Flask:
         app,
         content_security_policy=CSP,
         content_security_policy_nonce_in=["script-src"],
+        # redirect https if we detect an SSL enabled server
+        force_https=app.config["SSL_REDIRECT"],
     )
-
-    # if behind a proxy, tell Flask to use a proxy fixer
-    if app.config.get("SSL_REDIRECT") is not None:
-        app.wsgi_app = ProxyFix(app.wsgi_app)
 
     # register blueprints
     from .main import main as main_blueprint
