bb6d61c7c0dd732fb9ec46ddb0d0296b235f80f9
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-25 09:49:33 +0530

    feat(wsgi): add wsgi file with deploy command

--- enc.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from app import config_app
-
-app = config_app("development")
---
+++ wsgi.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import os
+
+from dotenv import load_dotenv
+from flask_migrate import Migrate, upgrade
+
+from app import config_app, db
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = config_app(os.environ.get("FLASK_CONFIG", "development"))
+Migrate(app, db)
+
+
+@app.cli.command()
+def deploy() -> None:
+    upgrade()
