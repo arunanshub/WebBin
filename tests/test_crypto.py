2a9521aaf2eb74208b1de1945bb2b0fb2f8c2fd3
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-28 21:54:57 +0530

    test(crypto): test compression threshold values

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -6,16 +6,28 @@ from hypothesis import strategies as st
 from app import crypto
 
 
-@given(paste=st.text(), title=st.text(), password=st.text())
+@given(
+    paste=st.text(),
+    title=st.text(),
+    password=st.text(),
+    compression_threshold_size=st.integers(min_value=1),
+)
 def test_encrypt_and_decrypt_paste(
     paste: str,
     title: str,
     password: str,
+    compression_threshold_size: int,
 ) -> None:
     raw_paste = crypto.RawPaste(title, paste)
+    encrypted_paste = crypto.encrypt_paste(
+        raw_paste,
+        password,
+        compression_threshold_size,
+    )
 
-    encrypted_paste = crypto.encrypt_paste(raw_paste, password)
+    if len(paste.encode()) >= compression_threshold_size:
+        assert encrypted_paste.is_compressed
+
     decrypted_paste = crypto.decrypt_paste(encrypted_paste, password)
-
     assert raw_paste.data == decrypted_paste.data
     assert raw_paste.title == decrypted_paste.title
