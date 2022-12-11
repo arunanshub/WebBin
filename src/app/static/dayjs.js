77d085088d6d31e0686b78ca51ce48c672610ffb
Arunanshu Biswas <mydellpc07@gmail.com>
2022-12-11 12:29:38 +0530

    feat(js): show locale aware paste creation date

--- src/app/static/dayjs.js
+++ src/app/static/dayjs.js
@@ -1,8 +1,7 @@
 dayjs.extend(window.dayjs_plugin_relativeTime);
 dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_localizedFormat);
 
-const elem = document.getElementById("relative_time");
-const localTime = dayjs().to(
-  dayjs(elem.getAttribute("data-relative-time")).utc(true).local()
-);
-document.getElementById("relative_time").textContent = localTime;
+const elem = document.getElementById("created_at");
+const localTime = dayjs(elem.getAttribute("data-created-at")).utc(true).local();
+elem.textContent = localTime.format("ll");
